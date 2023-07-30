8cb0ec99db33924271cc0689ef72d6422b6ea59d
Anil Gurau <gurau.anil@gmail.com>
2023-07-30 20:04:59 +0545

    identity setup

---
+++ LoginSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LoginSystem.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+    }
+}
--- LoginSystem/Program.cs
+++ LoginSystem/Program.cs
@@ -1,6 +1,15 @@
+using LoginSystem.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -18,6 +27,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
